63ea699972404fc029cef5ae919aecc1c12cfdda
Henri <henri.williams@capgemini.com>
2019-06-15 12:39:21 +0100

    Articles to json now working!

--- ai/sentance_splitter.py
+++ ai/sentance_splitter.py
@@ -3,12 +3,32 @@ import nltk.data
 from nltk.tokenize import word_tokenize
 import __future__
 
-tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+articles = []
+
+def get_articles_json(articles):
+    articles_json = []
+    printable = set(string.printable)
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    for article in articles:
+        # Convert to lower and remove non-ascii chars
+        article_cont = filter(lambda x: x in printable, str(article)).lower()
+        # Use the nltk sentence splitter to get the sentences
+        sentences = tokenizer.tokenize(article_cont)
+        # Split the sentences to words
+        src = []
+        for sentence in sentences:
+            s_list = word_tokenize(sentence)
+            src.append(s_list)
+        article_json = {
+            "src" : src,
+            "tgt" : []
+        }
+        articles_json.append(article_json)
+    return articles_json
+
+articles = []
 fp = open("test.txt")
 data = fp.read()
-printable = set(string.printable)
-data = filter(lambda x: x in printable, data)
-sentences = tokenizer.tokenize(data)
-for sentence in sentences:
-    print(word_tokenize(sentence))
-    print("\n")
+articles.append(data)
+print(str(get_articles_json(articles)))
+        
