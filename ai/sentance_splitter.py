6e24b782c4f56b9aaafc9b43a94d5c8a6a9d460d
Henri <henri.williams@capgemini.com>
2019-06-15 13:08:05 +0100

    Trying new splitter

--- ai/sentance_splitter.py
+++ ai/sentance_splitter.py
@@ -1,6 +1,5 @@
 import string
-import nltk.data
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 import __future__
 
 articles = []
@@ -8,12 +7,11 @@ articles = []
 def get_articles_json(articles):
     articles_json = []
     printable = set(string.printable)
-    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
     for article in articles:
         # Convert to lower and remove non-ascii chars
         article_cont = filter(lambda x: x in printable, str(article)).lower()
-        # Use the nltk sentence splitter to get the sentences
-        sentences = tokenizer.tokenize(article_cont)
+        # Split the sentences
+        sentences = sent_tokenize(article_cont)
         # Split the sentences to words
         src = []
         for sentence in sentences:
