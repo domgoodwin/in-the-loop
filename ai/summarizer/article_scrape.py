e1a0e0cd76141922bb765a5640501c850a7abb61
Williams <henri.williams@capgemini.com>
2019-06-24 20:09:32 +0100

    Added code to deal with URL for QA

---
+++ ai/summarizer/article_scrape.py
@@ -0,0 +1,15 @@
+import re
+from goose3 import Goose
+from requests import get
+
+def get_article_content(url):
+  response = get(url)
+  extractor = Goose()
+  article = extractor.extract(raw_html=response.content)
+  title = article.title
+  text = article.cleaned_text
+  # Remove non-ascii characters
+  text = re.sub(r'[^\x00-\x7F]+',' ', text)
+  return title, text
+
+print(get_article_content('https://www.dailymail.co.uk/news/article-7170867/Carrie-Symonds-scared-home-recorded-political-stich-neighbours.html'))
